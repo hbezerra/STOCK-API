41e10b1c5991e3de157049143b09d59bf09f6cd4
hbezerra <hugobezerradev@email.com>
2025-08-06 17:48:07 -0300

    Feat : Creating the repository and product exception ;

---
+++ src/main/java/com/stock/exception/ProductException.java
@@ -0,0 +1,9 @@
+package com.stock.exception;
+
+public class ProductException extends RuntimeException{
+
+    public ProductException(String msg){
+        super(msg);
+    }
+
+}
---
+++ src/main/java/com/stock/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.stock.repository;
+
+import com.stock.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    boolean existsByName(String name);
+}
