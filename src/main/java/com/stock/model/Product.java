b193f5550fc162d8c196b1cca1d9403016573e37
hbezerra <hugobezerradev@email.com>
2025-08-06 17:44:29 -0300

    Feat : Intial config and creating the product entity ;

---
+++ src/main/java/com/stock/model/Product.java
@@ -0,0 +1,23 @@
+package com.stock.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name ;
+    private String description ;
+    private Integer quantity ;
+
+}
